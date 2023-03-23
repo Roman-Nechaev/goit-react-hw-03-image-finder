09585786ef5a27f5787e654b30535e3b4fed69d7
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-23 22:18:00 +0200

    or

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div>
+          <p>Hellow</p>
+        </div>
+      </>
+    );
+  }
+}
